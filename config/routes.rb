7625ca1639b7c66376fd176781b6398c589e637d
div-div <numatachiakii@gmail.com>
2020-07-31 20:59:49 +0900

    set the registration/login views and function

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions:      'users/sessions',
+  }
+  devise_scope :user do
+    get 'profiles', to: 'users/registrations#new_profile'
+    post 'profiles', to: 'users/registrations#create_profile'
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+  root to: "home#index"
+  # resources :users
+  # get 'new_user_session_path', to: 'sessions#new'
+  # post 'user_session_path', to: 'sessions#create'
 end
