17e20bc75c2bfa2adf76153e8906b8ef555eb3f7
div-div <numatachiakii@gmail.com>
2020-08-10 16:22:40 +0900

    basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,23 @@
 class ApplicationController < ActionController::Base
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+
 
   def after_sign_in_path_for(resource)
     root_path(resource)
   end
 
+  def production?
+      Rails.env.production?
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
 
-  protected
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
-  #
-  # end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,4 @@
 server '54.95.157.216', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
